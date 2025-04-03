df9fc5ff241f34380a6efbed43d3e4fe148f833f
Yusuke-Yamasaki-555 <yuu5364yuu@gmail.com>
2025-04-04 01:20:44 +0900

    ロボットモデルを更新。Ver1のハードとほぼ同じ寸法、質量になった。慣性も良好

--- genesis/simple_model/spawn_simple_model.py
+++ genesis/simple_model/spawn_simple_model.py
@@ -1,11 +1,14 @@
+import os
+os.environ["PYOPENGL_PLATFORM"] = "glx"
+
 import genesis as gs
-gs.init(backend=gs.gpu)
+gs.init(backend=gs.cpu)
 
 scene = gs.Scene(show_viewer=True)
 plane = scene.add_entity(gs.morphs.Plane())
 franka = scene.add_entity(
-  # gs.morphs.MJCF(file='xml/franka_emika_panda/panda.xml'),
-  gs.morphs.URDF(file='../../robot_description/simple_model/simple_model.urdf', fixed=False),
+  #gs.morphs.MJCF(file='xml/franka_emika_panda/panda.xml'),
+  gs.morphs.URDF(file='../../robot_description/urdf_simple-model/simple_model.urdf', fixed=False),
 )
 
 scene.build()
