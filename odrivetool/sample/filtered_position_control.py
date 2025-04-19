c7eea162ef97f56ed30c48745aba02ba1f472de9
HarukiOgawa1 <s20c1028uk@s.chibakoudai.jp>
2025-04-19 14:42:10 +0900

    add processing after the program ends

--- odrivetool/sample/filtered_position_control.py
+++ odrivetool/sample/filtered_position_control.py
@@ -1,6 +1,8 @@
 import odrive
 import time
 import pygame
+import signal
+import sys
 
 odrv0 = odrive.find_any()
 
@@ -17,6 +19,15 @@ pygame.display.set_caption("ODrive Position Control")
 # 初期位置
 current_position = 0
 
+# Ctrl+Cなどの例外処理
+def signal_handler(sig, frame):
+    print('Exiting...')
+    odrv0.axis0.requested_state = 1  # プログラムが途中で止まった際に設定するコード
+    pygame.quit()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
 # メインループ
 running = True
 clock = pygame.time.Clock()
@@ -29,12 +40,12 @@ while running:
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_UP]:
-        current_position += 0.5
+        current_position += 1.0
         odrv0.axis0.controller.input_pos = current_position
         print(f'Position: {current_position}')
         time.sleep(0.1)  # 連続入力を防止
     elif keys[pygame.K_DOWN]:
-        current_position -= 0.5
+        current_position -= 1.0
         odrv0.axis0.controller.input_pos = current_position
         print(f'Position: {current_position}')
         time.sleep(0.1)
